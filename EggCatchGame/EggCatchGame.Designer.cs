05702e6b05653636f8f22d2fa9f55f9a0c8b8b3b
hikaru <hikaru@LAPTOP-84Q2UCPT>
2023-12-25 23:45:24 +0900

    プロジェクト ファイルを追加します。

---
+++ EggCatchGame/EggCatchGame.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace EggCatchGame
+{
+    partial class frmEggCatchGame
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEggCatchGame));
+            this.btnBegin = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnCenter = new System.Windows.Forms.Button();
+            this.lblCatcher = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblEgg = new System.Windows.Forms.Label();
+            this.lblCrushedEgg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBegin
+            // 
+            this.btnBegin.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.btnBegin.Location = new System.Drawing.Point(677, 12);
+            this.btnBegin.Name = "btnBegin";
+            this.btnBegin.Size = new System.Drawing.Size(111, 43);
+            this.btnBegin.TabIndex = 0;
+            this.btnBegin.Text = "開始";
+            this.btnBegin.UseVisualStyleBackColor = true;
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Enabled = false;
+            this.btnReset.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.btnReset.Location = new System.Drawing.Point(677, 61);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(111, 43);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "リセット";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("游ゴシック", 14F);
+            this.btnRight.Location = new System.Drawing.Point(476, 321);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(123, 44);
+            this.btnRight.TabIndex = 2;
+            this.btnRight.Text = "▶";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("游ゴシック", 14F);
+            this.btnLeft.Location = new System.Drawing.Point(218, 321);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(123, 44);
+            this.btnLeft.TabIndex = 3;
+            this.btnLeft.Text = "◀";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnCenter
+            // 
+            this.btnCenter.Font = new System.Drawing.Font("游ゴシック", 14F);
+            this.btnCenter.Location = new System.Drawing.Point(347, 321);
+            this.btnCenter.Name = "btnCenter";
+            this.btnCenter.Size = new System.Drawing.Size(123, 44);
+            this.btnCenter.TabIndex = 4;
+            this.btnCenter.Text = "●";
+            this.btnCenter.UseVisualStyleBackColor = true;
+            this.btnCenter.Click += new System.EventHandler(this.btnCenter_Click);
+            // 
+            // lblCatcher
+            // 
+            this.lblCatcher.BackColor = System.Drawing.Color.GreenYellow;
+            this.lblCatcher.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCatcher.Location = new System.Drawing.Point(347, 236);
+            this.lblCatcher.Margin = new System.Windows.Forms.Padding(3);
+            this.lblCatcher.Name = "lblCatcher";
+            this.lblCatcher.Size = new System.Drawing.Size(123, 23);
+            this.lblCatcher.TabIndex = 5;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("MS UI Gothic", 22F);
+            this.lblStatus.Location = new System.Drawing.Point(12, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(175, 30);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "ゲーム開始前";
+            // 
+            // lblEgg
+            // 
+            this.lblEgg.BackColor = System.Drawing.Color.White;
+            this.lblEgg.Image = ((System.Drawing.Image)(resources.GetObject("lblEgg.Image")));
+            this.lblEgg.Location = new System.Drawing.Point(382, 12);
+            this.lblEgg.Margin = new System.Windows.Forms.Padding(3);
+            this.lblEgg.Name = "lblEgg";
+            this.lblEgg.Size = new System.Drawing.Size(50, 50);
+            this.lblEgg.TabIndex = 8;
+            // 
+            // lblCrushedEgg
+            // 
+            this.lblCrushedEgg.BackColor = System.Drawing.Color.White;
+            this.lblCrushedEgg.Image = ((System.Drawing.Image)(resources.GetObject("lblCrushedEgg.Image")));
+            this.lblCrushedEgg.Location = new System.Drawing.Point(738, 318);
+            this.lblCrushedEgg.Margin = new System.Windows.Forms.Padding(3);
+            this.lblCrushedEgg.Name = "lblCrushedEgg";
+            this.lblCrushedEgg.Size = new System.Drawing.Size(50, 50);
+            this.lblCrushedEgg.TabIndex = 9;
+            this.lblCrushedEgg.Visible = false;
+            // 
+            // frmEggCatchGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 377);
+            this.Controls.Add(this.lblCrushedEgg);
+            this.Controls.Add(this.lblEgg);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblCatcher);
+            this.Controls.Add(this.btnCenter);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnBegin);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 416);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 416);
+            this.Name = "frmEggCatchGame";
+            this.Text = "エッグキャッチゲーム";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBegin;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnCenter;
+        private System.Windows.Forms.Label lblCatcher;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblEgg;
+        private System.Windows.Forms.Label lblCrushedEgg;
+    }
+}
+
---
+++ EggCatchGame/EggCatchGame.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace EggCatchGame
+{
+    public partial class frmEggCatchGame : Form
+    {
+        public frmEggCatchGame()
+        {
+            InitializeComponent();
+        }
+
+        //卵を落とす位置
+        string _dropLocation = "";
+
+        //卵を落とす速度
+        int _dropSpeed = 1000;
+
+        //キャッチ成功フラグ
+        bool _couldCaught = false;
+
+        //ゲームオーバーフラグ
+        bool _isGameOver = false;
+
+        //リセットフラグ
+        bool _isReset = false;
+
+        //開始ボタンが押されたときに行う処理
+        private void btnBegin_Click(object sender, EventArgs e)
+        {
+            //各コントロールと変数を開始状態にする
+            lblStatus.Text = "ゲーム開始";
+            btnBegin.Enabled = false;
+            btnReset.Enabled = true;
+            _isReset = false;
+
+            //卵を落とすタスクを開始する
+            Task _taskDropEgg = new Task(DropEgg);
+            _taskDropEgg.Start();
+
+            //ゲームオーバーフラグを監視するタスクを開始する
+            Task _taskMonitorGameOver = new Task(MonitorGameOver);
+            _taskMonitorGameOver.Start();
+        }
+
+        //リセットボタンが押されたときに行う処理
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //各コントロールと変数をリセット状態にする
+            _isReset = true;
+            btnBegin.Enabled = true;
+            btnReset.Enabled = false;
+            _isGameOver = false;
+            lblCatcher.Location = new Point(347, 236);
+            lblStatus.Text = "ゲーム開始前";
+            lblEgg.Visible = true;
+            lblCrushedEgg.Visible = false;
+            lblEgg.Location = new Point(382, 12);
+            _dropSpeed = 1000; 
+        }
+
+        //中央ボタンが押されたときに行う処理
+        private void btnCenter_Click(object sender, EventArgs e)
+        {
+            lblCatcher.Location = new Point(347, 236);
+        }
+
+        //右ボタンが押されたときに行う処理
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            lblCatcher.Location = new Point(476, 236);
+        }
+
+        //左ボタンが押されたときに行う処理
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            lblCatcher.Location = new Point(218, 236);
+        }
+
+        //卵を落とす処理
+        private void DropEgg()
+        {
+            for(int _cnt = 0; _cnt<10; _cnt++)
+            {
+                //卵を落とす位置をランダムに決める
+                _dropLocation = GetRandomLocation();
+
+                if(_dropLocation == "中央")
+                {
+                    //中央から落とす(リセットボタンが押されたら処理を抜ける)
+                    if (_isReset == true){ break; }
+                    Invoke(new Action(MoveCenterFirstDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveCenterSecondDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveCenterThirdDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveCenterForthDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    //クッションが卵の下にあるか判定し、なければゲームオーバーにする
+                    Invoke(new Action(JudgeCenterCatch));
+                    if (_isGameOver == true){ break; }
+                }
+                else if(_dropLocation == "右")
+                {
+                    //右から落とす(リセットボタンが押されたら処理を抜ける)
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveRightFirstDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveRightSecondDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveRightThirdDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveRightForthDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    //クッションが卵の下にあるか判定し、なければゲームオーバーにする
+                    Invoke(new Action(JudgeRightCatch));
+                    if (_isGameOver == true){ break; }
+                }
+                else if(_dropLocation == "左")
+                {
+                    //左から落とす(リセットボタンが押されたら処理を抜ける)
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveLeftFirstDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveLeftSecondDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveLeftThirdDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true) { break; }
+                    Invoke(new Action(MoveLeftForthDropLocation));
+                    System.Threading.Thread.Sleep(_dropSpeed);
+                    if (_isReset == true){ break; }
+                    //クッションが卵の下にあるか判定し、なければゲームオーバーにする
+                    Invoke(new Action(JudgeLeftCatch));
+                    if (_isGameOver == true){ break; }
+                }
+                //卵を落とす速度を上げる
+                _dropSpeed -= 100;
+            }
+
+            //10回連続でキャッチに成功した場合、ステータスを「ゲームクリア」にする
+            if(_isGameOver == false && _isReset == false)
+            {
+                Invoke(new Action(ChangeStatusGameClear));
+            }
+        }
+
+        //配列から要素をランダムに取り出す
+        private string GetRandomLocation()
+        {
+            string[] _dropLocations = new string[3] { "中央", "右", "左" };
+            Random random = new Random();
+            return _dropLocations[random.Next(_dropLocations.Length)];
+        }
+
+        //卵を中央から落とす場合の1番目の位置
+        private void MoveCenterFirstDropLocation()
+        {
+            lblEgg.Location = new Point(382, 12);
+        }
+
+        //卵を中央から落とす場合の2番目の位置
+        private void MoveCenterSecondDropLocation()
+        {
+            lblEgg.Location = new Point(382, 68);
+        }
+
+        //卵を中央から落とす場合の3番目の位置
+        private void MoveCenterThirdDropLocation()
+        {
+            lblEgg.Location = new Point(382, 124);
+        }
+
+        //卵を中央から落とす場合の4番目の位置
+        private void MoveCenterForthDropLocation()
+        {
+            lblEgg.Location = new Point(382, 180);
+        }
+
+        //卵を右から落とす場合の1番目の位置
+        private void MoveRightFirstDropLocation()
+        {
+            lblEgg.Location = new Point(511, 12);
+        }
+
+        //卵を右から落とす場合の2番目の位置
+        private void MoveRightSecondDropLocation()
+        {
+            lblEgg.Location = new Point(511, 68);
+        }
+
+        //卵を右から落とす場合の3番目の位置
+        private void MoveRightThirdDropLocation()
+        {
+            lblEgg.Location = new Point(511, 124);
+        }
+
+        //卵を右から落とす場合の4番目の位置
+        private void MoveRightForthDropLocation()
+        {
+            lblEgg.Location = new Point(511, 180);
+        }
+
+        //卵を左から落とす場合の1番目の位置
+        private void MoveLeftFirstDropLocation()
+        {
+            lblEgg.Location = new Point(253, 12);
+        }
+
+        //卵を左から落とす場合の2番目の位置
+        private void MoveLeftSecondDropLocation()
+        {
+            lblEgg.Location = new Point(253, 68);
+        }
+
+        //卵を左から落とす場合の3番目の位置
+        private void MoveLeftThirdDropLocation()
+        {
+            lblEgg.Location = new Point(253, 124);
+        }
+
+        //卵を左から落とす場合の4番目の位置
+        private void MoveLeftForthDropLocation()
+        {
+            lblEgg.Location = new Point(253, 180);
+        }
+
+        //中央でキャッチできたかどうかを判断する
+        private void JudgeCenterCatch()
+        {
+            if (lblCatcher.Location.X == 347 && lblCatcher.Location.Y == 236)
+            {
+                _couldCaught = true;
+            }
+            else
+            {
+                _couldCaught = false;
+            }
+            //キャッチできなかった場合、壊れた卵を中央に表示する
+            if (_couldCaught == false)
+            {
+                lblCrushedEgg.Location = new Point(382, 265);
+                lblCrushedEgg.Visible = true;
+                _isGameOver = true;
+            }
+        }
+
+        //右でキャッチできたかどうかを判断する
+        private void JudgeRightCatch()
+        {
+            if (lblCatcher.Location.X == 476 && lblCatcher.Location.Y == 236)
+            {
+                _couldCaught = true;
+            }
+            else
+            {
+                _couldCaught = false;
+            }
+            //キャッチできなかった場合、壊れた卵を右に表示する
+            if (_couldCaught == false)
+            {   
+                lblCrushedEgg.Location = new Point(511, 265);
+                lblCrushedEgg.Visible = true;
+                _isGameOver = true;
+            }
+        }
+
+        //左でキャッチできたかどうかを判断する
+        private void JudgeLeftCatch()
+        {
+            if (lblCatcher.Location.X == 218 && lblCatcher.Location.Y == 236)
+            {
+                _couldCaught = true;
+            }
+            else
+            {
+                _couldCaught = false;
+            }
+            //キャッチできなかった場合、壊れた卵を左に表示する
+            if (_couldCaught == false)
+            {
+                lblCrushedEgg.Location = new Point(253, 265);
+                lblCrushedEgg.Visible = true;
+                _isGameOver = true;
+            }
+        }
+
+        //ゲームオーバーフラグを監視する
+        private void MonitorGameOver()
+        {
+            while(_isGameOver == false)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+            //ゲームオーバーフラグが立った場合、卵を非表示にし、ステータスを「ゲームオーバー」にする
+            Invoke(new Action(HideEgg));
+            Invoke(new Action(ChangeStatusGameOver));
+        }
+
+        //卵を非表示にする
+        private void HideEgg()
+        {
+            lblEgg.Visible = false;
+        }
+
+        //ステータスを「ゲームオーバー」にする
+        private void ChangeStatusGameOver()
+        {
+            lblStatus.Text = "ゲームオーバー";
+        }
+
+        //ステータスを「ゲームクリア」にする
+        private void ChangeStatusGameClear()
+        {
+            lblStatus.Text = "ゲームクリア!";
+        }
+    }
+}
